e411ca44e91550c6597907b999fdd505de25b5e6
Wasym Shykh <creekgem@outlook.com>
2018-07-20 08:16:31 +0500

    Added test for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -6,37 +6,49 @@ const {Todo} = require('./../models/todo');
 
 describe('POST /todos', ()=> {
 
+
+    const todos = [
+        {
+            text: 'first test todo'
+        }, 
+        {
+            text: 'Second test todo'
+        }
+    ]
+
     beforeEach((done)=>{
 
-        Todo.remove({}).then(()=>done());
+        Todo.remove({}).then(()=>{
+            Todo.insertMany(todos);
+        }).then(()=>done());
 
     })
 
 
-    // it('should create a new todo',(done)=>{
+    it('should create a new todo',(done)=>{
 
-    //     let text = 'Testing todo text';
+        let text = 'Testing todo text';
 
-    //     request(app)
-    //     .post('/todos')
-    //     .send({text})
-    //     .expect(200)
-    //     .expect((res)=>{
-    //         expect(res.body.text).toBe(text);
-    //     })
-    //     .end((err,res)=>{
-    //         if(err){
-    //             return done(err);
-    //         }        
+        request(app)
+        .post('/todos')
+        .send({text})
+        .expect(200)
+        .expect((res)=>{
+            expect(res.body.text).toBe(text);
+        })
+        .end((err,res)=>{
+            if(err){
+                return done(err);
+            }        
 
-    //         Todo.find().then((todos)=>{
-    //             expect(todos.length).toBe(1);
-    //             expect(todos[0].text).toBe(text);
-    //             done();
-    //         }).catch(err => done(err))
-    //     })
+            Todo.find({text}).then((todos)=>{
+                expect(todos.length).toBe(1);
+                expect(todos[0].text).toBe(text);
+                done();
+            }).catch(err => done(err))
+        })
 
-    // });
+    });
 
     it('should not create a new todo with empty text',(done)=>{
 
@@ -51,7 +63,7 @@ describe('POST /todos', ()=> {
 
             Todo.find().then((todos)=>{
                 
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(2);
                 done();
 
             }).catch((err)=>done(err))
@@ -59,5 +71,21 @@ describe('POST /todos', ()=> {
         });
 
     });
+
+    describe('GET /todos', ()=>{
+
+        it('should get all todos', (done)=>{
+
+            request(app)
+            .get('/todos')
+            .expect(200)
+            .expect(res=>{
+                expect(res.body.todos.length).toBe(2)
+            })
+            .end(done())
+
+        })
+
+    })
 
 })
