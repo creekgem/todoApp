1687241542297f86320be3786d08fc237f5ffd80
Wasym Shykh <creekgem@outlook.com>
2018-07-20 22:04:41 +0500

    Added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose')
 const {Todo} = require('./models/todo')
@@ -25,15 +26,38 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res)=> {
     Todo.find().then(todos=>{
-        res.send({todos})
+        res.send({todos});
     }, (e)=>{
-        res.status(400).send(e)
-    })
-})
+        res.status(400).send(e);
+    });
+});
+
+app.get('/todos/:id', (req, res)=>{
+
+    let {id} = req.params;    
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send({});
+    }
+
+    Todo.findById(id).then((todo)=>{
+
+        if(!todo) {
+            return res.status(404).send({});
+        } 
+        
+        res.send({todo});   
+
+    }).catch(e=>{
+        res.status(400).send({});
+    });
+
+
+});
 
 
 app.listen(3000, ()=>{
     console.log('Started on port 3000');
-})
+});
 
 module.exports = {app}
