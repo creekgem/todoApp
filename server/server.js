5bb4908b9acde5d36404a2234339a1d3486f935e
Wasym Shykh <creekgem@outlook.com>
2018-07-23 08:34:27 +0500

    Added GET /user/me & authenticate middleware

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const {ObjectID} = require('mongodb');
 const {mongoose} = require('./db/mongoose')
 const {Todo} = require('./models/todo')
 const {User} = require('./models/user')
+const {authenticate} = require('./middleware/authenticate');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -124,6 +125,13 @@ app.post('/users', (req, res)=>{
     });
 
 });
+
+
+app.get('/users/me', authenticate, (req, res)=>{
+    
+    res.send(req.user);
+
+})
 
 
 app.listen(port, ()=>{
